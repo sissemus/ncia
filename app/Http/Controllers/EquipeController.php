b83942bc878bdae2537b03fedcb69e0cfb0b16d5
euclidesbarbosa <euclidesbarbosa@gmail.com>
2026-09-15 09:44:44 -0300

    Correções na Equipe

--- app/Http/Controllers/EquipeController.php
+++ app/Http/Controllers/EquipeController.php
@@ -28,7 +28,7 @@ class EquipeController extends Controller
         return view('equipe.equipe_view', compact('tiposProfissional', 'profissionais', 'tiposVeiculo'));
     }
 
-    public function inserir(Request $request)
+    public function inserir(EquipeCreateRequest $request)
     {
 
         $equipes = [];
@@ -36,12 +36,25 @@ class EquipeController extends Controller
         $EQUIPE_ID = null;
 
         $USUARIO_ID_CAD = Auth::id();
+
+        // informações sobre o veículo
+        $veiculo = Veiculo::findOrFail($request->input('VEICULO_ID'));
+        $tipoVeiculo = $veiculo->TG_TIPO_VEICULO_ID;
+
+        $equipe = new Equipe($request->input());
+        $equipe->EQUIPE_ATIVO = 1;
+        $equipe->EQUIPE_DATA = now()->format('Y-m-d');
+        $equipe->USUARIO_ID_CAD = $USUARIO_ID_CAD;
+
+        DB::beginTransaction();
+
+        // salvando equipe
+        $equipe->save();
+
+        $EQUIPE_ID = $equipe->EQUIPE_ID;
         
         $int = 0;
-
-        $veiculoTipo = [];
-        
-        
+  
         //para o tipo de veículo == 1
         $tiposProfissional_1 = [1, 2, 4];
         $tiposProfissional_1_compare = [];
@@ -49,55 +62,16 @@ class EquipeController extends Controller
         //para o tipo de veículo == 2
         $tiposProfissional_2 = [1, 2, 3, 4];
         $tiposProfissional_2_compare = [];
-
-        DB::beginTransaction();
         
-        foreach ($request->input() as $dados) {
-
-            if($dados['VEICULO_ID'] == null){
-                DB::rollBack();
-                throw ValidationException::withMessages([
-                    'VEICULO_ID' =>
-                        'Veículo não selecionado!'
-                ]);
-            }
-
-            if($dados['EQUIPE_TURNO'] == null){
-                DB::rollBack();
-                throw ValidationException::withMessages([
-                    'EQUIPE_TURNO' =>
-                        'Turno não selecionado!'
-                ]);
-            }
-
-            if($dados['TG_TIPO_PROFISSIONAL_ID'] == null){
-                DB::rollBack();
-                throw ValidationException::withMessages([
-                    'TG_TIPO_PROFISSIONAL_ID' =>
-                        'Tipo de profissional não informado!'
-                ]);
-            }
-
-            if($dados['PROFISSIONAL_ID'] == null){
-                DB::rollBack();
-                throw ValidationException::withMessages([
-                    'PROFISSIONAL_ID' =>
-                        'Profissional não informado!'
-                ]);
-            }
+        foreach ($request->input('equipeProfissionais') as $dados) {
 
             $PROFISSIONAL_ID = $dados['PROFISSIONAL_ID'];
-
-            $VEICULO_ID = $dados['VEICULO_ID'];
 
             $profissional = Profissional::findOrFail($PROFISSIONAL_ID);
 
             $tipoProfissional  = $profissional->TG_TIPO_PROFISSIONAL_ID;
 
-            $veiculo = Veiculo::findOrFail($VEICULO_ID);
-
-            $tipoVeiculo = $veiculo->TG_TIPO_VEICULO_ID;
-
+            // crio um array temporário para armazenar o tipo do veículo
             if($tipoVeiculo == 1){
                 array_push($tiposProfissional_1_compare, $tipoProfissional);
             }
@@ -105,21 +79,9 @@ class EquipeController extends Controller
                 array_push($tiposProfissional_2_compare, $tipoProfissional);
             }
 
-            if($int == 0){
-                    
-                $equipe = new Equipe($dados);
-                $equipe->EQUIPE_ATIVO = 1;
-                $equipe->EQUIPE_DATA = now()->format('Y-m-d');
-                $equipe->USUARIO_ID_CAD = $USUARIO_ID_CAD;
-                
-                $equipe->save();
-
-                $EQUIPE_ID = $equipe->EQUIPE_ID;
-
-            }
-
             $int++;
 
+            // informações da equipeProfissional
             $equipeProfissional = new EquipeProfissional($dados);
 
             // para cada profissional
@@ -128,17 +90,18 @@ class EquipeController extends Controller
             $equipeProfissional->EQUIPE_PROFISSIONAL_ATIVO = 1;
 
             $equipeProfissional->USUARIO_ID_CAD = $USUARIO_ID_CAD;
-            
+
             $equipeProfissional->save();
     
         }
 
-        //validando
+        //validando se a equipe está completa
         if($tipoVeiculo == 1){
 
             sort($tiposProfissional_1);
             sort($tiposProfissional_1_compare);
 
+            // comparando por tipo de equipe
             if($tiposProfissional_1 != $tiposProfissional_1_compare){
                 throw ValidationException::withMessages([
                     'TG_TIPO_PROFISSIONAL_ID' =>
--- app/Http/Requests/Equipe/EquipeCreateRequest.php
+++ app/Http/Requests/Equipe/EquipeCreateRequest.php
@@ -4,110 +4,29 @@ namespace App\Http\Requests\Equipe;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Validation\Validator;
 
 class EquipeCreateRequest extends FormRequest
 {
-
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
     public function authorize()
     {
         return Auth::check();
     }
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-
     public function rules()
     {
-        $rules = [];
-
-        foreach($this->all() as $indice => $dados){
-
-            $rules[$indice . '.VEICULO_ID'] = [
-                'required', 'numeric',
-            ];
-
-            $rules[$indice . '.EQUIPE_TURNO'] =[
-                'required'
-            ];
-
-            $rules[$indice . '.TG_TIPO_VEICULO_ID'] =[
-                'required', 'numeric', 'in:1,2',
-            ];
-
-            $rules[$indice . '.TG_TIPO_PROFISSIONAL_ID'] =[
-                'required', 'numeric',
-            ];
-
-            /**
-             * TIPO VEICULO = 1, 2, ou 4
-             * 1 = condutor
-             * 2 = enfermeiro
-             * 3 = medico
-             * 4 = técnico em enfermagem
-             * */
-             if(($dados['TG_TIPO_VEICULO_ID'] ?? null) == 1){
-
-                $rules[$indice . '.TG_TIPO_VEICULO_ID'][] = 'in:1,2,4';
-
-             }
-
-            /**
-             * TIPO VEICULO = 1, 2, 3 ou 4
-             * 
-             * */
-             if(($dados['TG_TIPO_VEICULO_ID'] ?? null) == 2){
-
-                $rules[$indice . '.TG_TIPO_VEICULO_ID'][] = 'in:1,2,3,4';
-
-             }
-
-             
-        }
-
-        return $rules;
-
+        return [
+            "VEICULO_ID" => ["required", "integer"],
+            "EQUIPE_TURNO" => ["required"],
+            "equipeProfissionais" => ["required", "array", "min:1"]
+        ];
     }
 
-    // public function withValidator(Validator $validator)
-    // {
-    //     $validator->after(function ($validator) {
-    //         if (empty($this->all())) {
-    //             $validator->errors()->add('geral', 'Dados incorretos para criar a equipe!');
-    //         }
-    //     });
-    // }
-    
-        public function messages()
-        {
-            $messages = [];
-
-            foreach ($this->all() as $indice => $dados) {
-                // Mensagens para o VEICULO_ID
-                $messages[$indice . '.VEICULO_ID.required'] = "O veículo do item " . ($indice + 1) . " é obrigatório.";
-                $messages[$indice . '.VEICULO_ID.numeric'] = "O veículo do item " . ($indice + 1) . " deve ser um número.";
-
-                // Mensagens para o EQUIPE_TURNO
-                $messages[$indice . '.EQUIPE_TURNO.required'] = "O turno da equipe do item " . ($indice + 1) . " é obrigatório.";
-
-                // Mensagens para o TG_TIPO_VEICULO_ID
-                $messages[$indice . '.TG_TIPO_VEICULO_ID.required'] = "O tipo de veículo do item " . ($indice + 1) . " é obrigatório.";
-                $messages[$indice . '.TG_TIPO_VEICULO_ID.numeric'] = "O tipo de veículo do item " . ($indice + 1) . " deve ser um número.";
-                $messages[$indice . '.TG_TIPO_VEICULO_ID.in'] = "O tipo de veículo selecionado para o item " . ($indice + 1) . " é inválido.";
-
-                // Mensagens para o TG_TIPO_PROFISSIONAL_ID
-                $messages[$indice . '.TG_TIPO_PROFISSIONAL_ID.required'] = "O tipo de profissional do item " . ($indice + 1) . " é obrigatório.";
-                $messages[$indice . '.TG_TIPO_PROFISSIONAL_ID.numeric'] = "O tipo de profissional do item " . ($indice + 1) . " deve ser um número.";
-            }
-
-            return $messages;
-        }
+    public function attributes()
+    {
+        return [
+            "VEICULO_ID" => "<b>VEÍCULO</b>",
+            "EQUIPE_TURNO" => "<b>TURNO</b>",
+            "equipeProfissionais" => "<b>PROFISSIONAIS SELECIONADOS</b>",
+        ];
+    }
 }
